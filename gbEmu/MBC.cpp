835c600f7f87ea894b208bbe2d244dd1f9f470fc
Michael-MD <mdei0001@student.monash.edu>
2024-01-09 02:23:37 +1100

    Created base class memory bank controllers

---
+++ gbEmu/MBC.cpp
@@ -0,0 +1,71 @@
+#include "MBC.hpp"
+
+MBC::MBC(uint8_t ROMSize, uint8_t RAMSize)
+{
+	// Convert sizes in game header to proper size in bytes
+
+	switch (ROMSize)
+	{
+	case 0x00:
+		nROMBanks = 2;
+		break;
+	case 0x01:
+		nROMBanks = 4;
+		break;
+	case 0x02:
+		nROMBanks = 8;
+		break;
+	case 0x03:
+		nROMBanks = 16;
+		break;
+	case 0x04:
+		nROMBanks = 32;
+		break;
+	case 0x05:
+		nROMBanks = 64;
+		break;
+	case 0x06:
+		nROMBanks = 128;
+		break;
+	case 0x52:
+		nROMBanks = 72;
+		break;
+	case 0x53:
+		nROMBanks = 80;
+		break;
+	case 0x54:
+		nROMBanks = 96;
+		break;
+	}
+
+	ROMSizeBytes = ROM_BANK_SIZE * nROMBanks;
+
+	switch (RAMSize)
+	{
+	case 0x00:
+		nRAMBanks = 0;
+		break;
+	case 0x01:
+	case 0x02:
+		nRAMBanks = 1;
+		break;
+	case 0x03:
+		nRAMBanks = 4;
+		break;
+	case 0x04:
+		nRAMBanks = 16;
+		break;
+	}
+
+	RAMSizeBytes = RAM_BANK_SIZE * nRAMBanks;
+
+	ROM = new uint8_t[ROMSizeBytes];
+	RAM = new uint8_t[RAMSizeBytes];
+
+}
+
+MBC::~MBC()
+{
+	delete[] ROM;
+	delete[] RAM;
+}
---
+++ gbEmu/MBC.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <cstdint>
+
+class MBC
+{
+public:
+	MBC(uint8_t ROMSize, uint8_t RAMSize);
+	~MBC();
+
+	uint8_t* ROM;
+	uint8_t* RAM;
+
+	const int ROM_BANK_SIZE = 16 * 1024;
+	const int RAM_BANK_SIZE = 8 * 1024;
+	int nROMBanks, nRAMBanks;
+	int ROMSizeBytes, RAMSizeBytes;
+
+	virtual void write(uint16_t addr, uint8_t data) = 0;
+	virtual uint8_t read(uint16_t addr) = 0;
+};
