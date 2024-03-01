a0ad51a6d5f56438c344e5c868a232bc36a42d6d
Michael-MD <mdei0001@student.monash.edu>
2024-03-01 17:35:49 +1100

    Generates empty window

--- gbEmu/gbEmu.cpp
+++ gbEmu/gbEmu.cpp
@@ -10,36 +10,7 @@
 
 int main(int argc, char* argv[])
 {
-    //GB gb("C:/Users/61481/Documents/code/gb/games/tetris.gb");
-    //GB gb("C:/Users/61481/Documents/code/gb/games/Boxxle (USA, Europe) (Rev 1).gb");
-    GB gb("C:/Users/61481/Documents/code/gb/games/kdl.gb");
-    //GB gb("C:/Users/61481/Documents/code/gb/games/F-1 Race (JUE) (V1.1) [!].gb");
-    //GB gb("C:/Users/61481/Documents/code/gb/games/Pokemon Red (UE) [S][!].gb");
-    //GB gb("C:/Users/61481/Documents/code/gb/games/mb.gb");
-    //GB gb("C:/Users/61481/Documents/code/gb/games/PR.gb");
-    //GB gb("C:/Users/61481/Documents/code/gb/games/Dr. Mario (World).gb");
- 
-    //GB gb("C:/Users/61481/Documents/code/gb/gb-test-roms-master/dmg_sound/rom_singles/04-sweep.gb");
-    //GB gb("C:/Users/61481/Documents/code/gb/gb-test-roms-master/cpu_instrs/individual/02-interrupts.gb");
-    //GB gb("C:/Users/61481/Documents/code/gb/gb-test-roms-master/cpu_instrs/individual/03-op sp,hl.gb");
-    //GB gb("C:/Users/61481/Documents/code/gb/gb-test-roms-master/cpu_instrs/individual/04-op r,imm.gb");
-    //GB gb("C:/Users/61481/Documents/code/gb/gb-test-roms-master/cpu_instrs/individual/05-op rp.gb");
-    //GB gb("C:/Users/61481/Documents/code/gb/gb-test-roms-master/cpu_instrs/individual/06-ld r,r.gb");
-    //GB gb("C:/Users/61481/Documents/code/gb/gb-test-roms-master/cpu_instrs/individual/07-jr,jp,call,ret,rst.gb");
-    //GB gb("C:/Users/61481/Documents/code/gb/gb-test-roms-master/cpu_instrs/individual/08-misc instrs.gb");
-    //GB gb("C:/Users/61481/Documents/code/gb/gb-test-roms-master/cpu_instrs/individual/09-op r,r.gb");
-    //GB gb("C:/Users/61481/Documents/code/gb/gb-test-roms-master/cpu_instrs/individual/10-bit ops.gb");
-    //GB gb("C:/Users/61481/Documents/code/gb/gb-test-roms-master/cpu_instrs/individual/11-op a,(hl).gb");
-    //GB gb("C:/Users/61481/Documents/code/gb/gb-test-roms-master/cpu_instrs/cpu_instrs.gb");
-    //GB gb("C:/Users/61481/Documents/code/gb/gb-test-roms-master/mem_timing/mem_timing.gb");
-    //GB gb("C:/Users/61481/Documents/code/gb/mts-20240108-1545-b5740e1/acceptance/instr/daa.gb");
-    //new GB("C:/Users/61481/Documents/code/gb/mts-20240108-1545-b5740e1/acceptance/ppu/intr_2_mode3_timing.gb");
-    //new GB("C:/Users/61481/Documents/code/gb/mts-20240108-1545-b5740e1/acceptance/ei_sequence.gb");
-
-    //GB gb("C:/Users/61481/Documents/code/gb/mealybug-tearoom-tests/m3_bgp_change.gb");
-    //GB gb("C:/Users/61481/Documents/code/gb/mealybug-tearoom-tests/m3_lcdc_bg_map_change.gb");
-    //GB gb("C:/Users/61481/Documents/code/gb/dmg-acid2.gb");
-
+    GB gb;
 
     return 0;
 }
